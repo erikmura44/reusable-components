d5bc47e45aaeafa12ef0e3243a22f97765c1ead5
Mura, Erik <erik.mura@vertafore.com>
2017-11-15 15:46:51 -0700

    added highlights

--- src/docs/CodeExample.js
+++ src/docs/CodeExample.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import hljs from 'highlight.js/lib/highlight'
-import javascript from 'highlight.js/lib/languages/javascript'
+import hljs from 'highlight.js/lib/highlight';
+import javascript from 'highlight.js/lib/languages/javascript';
 
 // This way is easy, but adds 170K gzipped to bundle since all langs are included.
 // import Highlight from 'react-highlight';
