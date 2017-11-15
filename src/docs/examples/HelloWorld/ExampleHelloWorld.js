03aad0d39f9a1e4baaa9ad92f66b1c8e48b01eb9
Mura, Erik <erik.mura@vertafore.com>
2017-11-15 15:41:01 -0700

    created hello component with index for export

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'erik/HelloWorld';
+
+/** Custom Message */
+export default function ExampleHelloWorld() {
+    return <HelloWorld message="People" />
+}
