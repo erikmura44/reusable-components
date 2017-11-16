797c8fbd2b5ef1bfb9e203f888375f65ea7af396
Mura, Erik <erik.mura@vertafore.com>
2017-11-16 13:58:25 -0700

    added textinput using label and required

---
+++ src/docs/examples/TextInput/ExampleOptional.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TextInput from '../../../components/TextInput/TextInput';
+
+/** Optional TextBox */
+
+export default class ExampleOptional extends React.Component {
+    render() {
+        return (
+            <TextInput  
+                htmlId="example-optional"
+                label="First Name"
+                name="firstname"
+                onChange={() => {}}
+            />
+        );
+    }
+}
